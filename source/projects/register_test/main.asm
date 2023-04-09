95eb051a5fb718ae7bbb45620a11e6cbbe25d820
Colin M <colinm22@iastate.edu>
2023-04-08 21:12:21 -0500

    Circuit

--- source/projects/register_test/main.asm
+++ source/projects/register_test/main.asm
@@ -1,14 +1,36 @@
 #bank rom
 
 main:
+    iml a, 0b10101010
+    imu a, 0b10101010
+
+    nop
+    nop
+    nop
+
+    xor a, a
     not a
-    not b
-    not c
-    not d
-    not e
-    not f
-    not stack
-    not flags
+    add a, a
+    not a
+    ; a contains 1
+
+    mov b, a
+    add b, a
+
+    mov c, b
+    add c, b
+
+    mov d, c
+    add d, c
+
+    mov e, d
+    add e, d
+
+    mov f, e
+    add f, e
+
+    imm a, interrupt
+    jmp a
 
 interrupt:
     imm a, .loop
