5329cef1e1b9d377557bd400f1a2c0c7b590cac2
Colin M <colinm22283@gmail.com>
2023-04-11 01:16:41 -0500

    Stack controller needs some work

--- source/projects/register_test/main.asm
+++ source/projects/register_test/main.asm
@@ -1,44 +1,9 @@
 #once
 
-#bank ram
-ram_func: #res 16
-
-#bank rom
-rom_func: ; f: return
-    imc a, 20
-    cmb a
-
-    imc c, 0
-    imm d, .loop - rom_func + ram_func
-    cma c
-    .loop:
-        adi c, 1
-
-        jlt d
-
-    jmp f
-
 main:
-    imm a, rom_func + 22
-    cmb a
-
-    imm a, rom_func
-    imm b, ram_func
-    imm d, .loop
-    cma a
-
-    .loop:
-        ldl c, a
-        wrl b, c
-
-        adi a, 1
-        adi b, 1
-
-        jlt d
-
-    imm a, ram_func
-    imm f, $ + 6
-    jmp a
+    imm a, 0xF00F
+    psu a
+    psl a
 
     imm a, interrupt
     jmp a
